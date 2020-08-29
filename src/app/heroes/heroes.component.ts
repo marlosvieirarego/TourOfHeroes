d4e0d591888b19295c2f61638aa6ec4290b69164
marlosvieirarego` <`vieira.marlos@gmail.com`>
2020-08-29 14:36:42 -0300

    Capitulo 4-Add service completo

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero' ; 
 import { HeroService } from '../hero.service' ;
-
+import { MessageService } from '../message.service' ;
 
 @Component({
   selector: 'app-heroes',
@@ -13,14 +13,15 @@ export class HeroesComponent implements OnInit {
   heroes: Hero [] ;
   selectedHero: Hero ;
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes() ;
   };
 
-  onSelect(hero: Hero) {
-    this.selectedHero = hero ;    
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero ;  
+    this.messageService.messages.push(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
